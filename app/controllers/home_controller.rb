08056e9ee5876c35a926e118339cc06a29d814bc
shayashi683 <shayashi683@gmail.com>
2020-06-06 21:03:07 +0900

    2020.06.06

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def top
+    @faculties_1 = Faculty.find(1)
+    @faculties_2 = Faculty.find(2)
+    @faculties_3 = Faculty.find(3)
   end
   def about
   end
