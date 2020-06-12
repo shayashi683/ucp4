1fba91f19232e11540b4fd1d3d7377fb432686bf
shayashi683 <shayashi683@gmail.com>
2020-06-12 21:40:44 +0900

    update categoreis mdel

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
   def top
-    @categories_1 = Category.find(1)
-    @categories_2 = Category.find(2)
-    @categories_3 = Category.find(3)
+    @categories_1 = Category.find(id) 
   end
+
   def about
   end
 end
--- db/migrate/20200612110233_change_faculties_to_categories.rb
+++
@@ -1,5 +0,0 @@
-class ChangeFacultiesToCategories < ActiveRecord::Migration[5.2]
-  def change
-    rename_table :faculties, :categories
-  end
-end
