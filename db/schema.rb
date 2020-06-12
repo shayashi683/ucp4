30f106a3451cacac2ca7dd193770b6d82282f206
shayashi683 <shayashi683@gmail.com>
2020-06-12 21:46:02 +0900

    drop model categories

--- db/schema.rb
+++ db/schema.rb
@@ -20,11 +20,6 @@ ActiveRecord::Schema.define(version: 2020_06_01_093358) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "elements_edus", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
