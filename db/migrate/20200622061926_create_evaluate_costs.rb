027d7dea50b071c3b6158ffa405afe8402603c7a
shayashi683 <shayashi683@gmail.com>
2020-06-24 01:30:53 +0900

    modify changes

--- db/migrate/20200622061926_create_evaluate_costs.rb
+++ db/migrate/20200622061926_create_evaluate_costs.rb
@@ -3,7 +3,7 @@ class CreateEvaluateCosts < ActiveRecord::Migration[5.2]
     create_table :evaluate_costs do |t|
       t.integer :colleges_faculties_id
       t.integer :tuition
-      t.integer :employment_rate
+      t.integer :future_income
       t.integer :scholarship
       t.decimal :cost_grade
 
